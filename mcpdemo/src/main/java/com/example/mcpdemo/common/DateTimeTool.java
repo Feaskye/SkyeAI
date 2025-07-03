968c1dd2bd4f82e454f93c0bec067c9f88bb18dd
wangrongfa <wangrongfa@hangzhouhaobo.com>
2025-07-04 07:29:06 +0800

    init ali mcp

--- mcpdemo/src/main/java/com/example/mcpdemo/common/DateTimeTool.java
+++ mcpdemo/src/main/java/com/example/mcpdemo/common/DateTimeTool.java
@@ -14,7 +14,8 @@ import java.util.Map;
 public class DateTimeTool {
 
     private static final Map<String,String> COUNTRY_MAP=Map.of(
-            "c1","2020-02-01 12:00:00",   "c2","2020-02-01 13:00:00"  );
+            "c1","2020-02-01 12:00:00",
+            "c2","2020-02-01 13:00:00"  );
  @Tool(description ="国家时间查询工具")
  public String getCurrentDateTimeByCountry(String country){
      return COUNTRY_MAP.getOrDefault(country,
